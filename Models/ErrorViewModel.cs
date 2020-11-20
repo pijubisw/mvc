725139668f7bcd5c4dd89858717861856f40ff23
Pijush Biswas <pijushatibm@gmail.com>
2020-11-20 17:26:25 +0530

    Intial commit to test public github

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace aks.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
